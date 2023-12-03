cd9d58d758502d8cf93a0795a6289b4fe07fc4a2
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-12-03 17:42:38 -0500

    Update constants

--- src/main.py
+++ src/main.py
@@ -2,27 +2,24 @@ import random
 
 import pyray
 
-NUM_PARTICLES = 5000
+WIDTH = 1000
+HEIGHT = 500
 
-WIDTH = 800
-HEIGHT = 400
+PARTICLE_SIZE = 25
 
-pyray.init_window(WIDTH, HEIGHT, "Hello")
-
-PARTICLE_SIZE = 10
+NUM_PARTICLES = 400
 
 M = WIDTH // PARTICLE_SIZE
 N = HEIGHT // PARTICLE_SIZE
 
 grid = [[[] for _ in range(N)] for _ in range(M)]
 
-# print(grid)
-
-# add random particles
 for _ in range(NUM_PARTICLES):
     random_i = random.randint(0, M - 1)
     random_j = random.randint(0, N - 1)
     grid[random_i][random_j].append(object())
+
+pyray.init_window(WIDTH, HEIGHT, "")
 
 while not pyray.window_should_close():
     pyray.begin_drawing()
