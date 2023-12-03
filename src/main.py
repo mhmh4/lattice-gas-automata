4b5bcad5e0ba9f2639519f9ee6e5024329d78592
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-12-03 16:27:57 -0500

    Add many random particles

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import random
 
 import pyray
 
+NUM_PARTICLES = 50
 
 WIDTH = 800
 HEIGHT = 400
@@ -17,10 +18,11 @@ grid = [[[] for _ in range(N)] for _ in range(M)]
 
 # print(grid)
 
-# add random particle
-random_i = random.randint(0, M - 1)
-random_j = random.randint(0, N - 1)
-grid[random_i][random_j].append(object())
+# add random particles
+for _ in range(NUM_PARTICLES):
+    random_i = random.randint(0, M - 1)
+    random_j = random.randint(0, N - 1)
+    grid[random_i][random_j].append(object())
 
 while not pyray.window_should_close():
     pyray.begin_drawing()
